d6b2dc1c1beaab5856e104deb86a25db0307e7b1
root <root@docker-test.c.mesos-test-142404.internal>
2016-09-10 14:47:23 +0000

    added subprocessOpen

--- readProcess.py
+++ readProcess.py
@@ -1,3 +1,9 @@
+import subprocess
+
+def subprocessOpen(command):
+    return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
 
 if __name__ == '__main__':
-    print 'start'
+    (stdoutdata, stderrdata) = subprocessOpen('ps -eo pid,lstart,cmd  | grep -v grep').communicate()
+    print stdoutdata
